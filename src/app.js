ba6522c5809403a18d41833dba8109c858418772
caizf <1553071468@qq.com>
2022-02-18 09:26:15 +0800

    添加

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { NewButton } from './components';
+import { Button } from 'antd';
+import './app.css'
 
 function App() {
   return (
-    <NewButton name="新按钮" />
+    <div>
+      <NewButton name="新按钮" />
+      <Button type="primary">按钮</Button>
+    </div>
+    
   )
 }
 
